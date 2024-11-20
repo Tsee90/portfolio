2b3d2adcb827a47ed8fdb87a621007c6b1366b4d
Tsee90 <179171024+Tsee90@users.noreply.github.com>
2024-11-20 18:09:31 -0500

    style: mobile view header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+const button = document.querySelectorAll('.button-github');
+button.forEach((butt) => {
+  butt.addEventListener('click', (event) => {
+    alert('github');
+  });
+});
